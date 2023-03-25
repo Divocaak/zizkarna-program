b2450ddcb11321bae6b4120db2ff7aaed353bf3d
Vojtěch divoký <divokyvojta@gmail.com>
2023-03-25 10:16:16 +0100

    json data fetch

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Roboto:wght@300;400;500&family=Unbounded:wght@400;500;700;800&display=swap"
+		rel="stylesheet"
+	/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</svelte:head>
+
+<slot />
+
+<!-- <style lang="scss">
+	:root {
+		--black: black;
+		--white: white;
+		--yellow: #f9d204;
+	}
+
+	:global(html) {
+		scroll-behavior: smooth;
+	}
+
+	:global(body) {
+		background-color: var(--black);
+		color: var(--white);
+	}
+
+	:global(.montserrat) {
+		font-family: 'Montserrat', sans-serif;
+	}
+
+	:global(.roboto) {
+		font-family: 'Roboto', sans-serif;
+	}
+
+	:global(.unbounded) {
+		font-family: 'Unbounded', cursive;
+	}
+
+	:global(.black-on-white) {
+		color: var(--black);
+		background-color: var(--white);
+	}
+
+	:global(.white-on-black) {
+		color: var(--white);
+		background-color: var(--black);
+	}
+</style> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import content from '$lib/content.json';
+	
+	var tags = content['tags'];
+	var events = content['events'];
+</script>
+
+<!-- TODO share btn (on click, find id, scroll to) -->
+<!-- TODO add to calendar -->
+{#each events as event}
+	<h1>{event.label}</h1>
+	<p>{event.date}</p>
+	<p>{event.cash},-&nbsp;Kč</p>
+	<p>{event.doors}</p>
+	<a href={event.fbEvent} class="btn btn-outline-primary"><i class="pe-2 bi bi-facebook"></i>událost na Facebooku</a>
+	{#if event.tickets != null}
+		<a href={event.tickets} class="btn btn-outline-secondary"><i class="pe-2 bi bi-ticket-perforated-fill"></i>Předprodej lístků</a>
+	{/if}
+	{#each event.tags as tag}
+    <!-- color:{tag.textColor}!important; -->
+		<span class="badge bg-secondary mx-1" style="background-color:red!important;">{tags[tag].label}</span>
+	{/each}
+{/each}
