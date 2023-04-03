6d67b73f7f8278fab9b129d58ddeed23b0203d69
Vojtěch divoký <divokyvojta@gmail.com>
2023-04-03 12:11:03 +0200

    placeholder img, no event btn, anim init

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -2,12 +2,26 @@
 	export var event;
 	export var tags;
 
+    import viewport from './useViewportAction';
+	import { fade, fly } from 'svelte/transition';
+
 	var dateFormatted = new Date(event.date).toLocaleDateString('cs-CZ', {});
 </script>
 
-<div class="row d-flex justify-content-center mx-5 px-5">
+<div
+	use:viewport
+	on:enterViewport={() => animate}
+	on:exitViewport={() => console.log(event.label + ' exit!')}
+	class="row d-flex justify-content-center mx-5 px-5"
+	in:fly={{ y: 200, duration: 2000 }}
+	out:fade
+>
 	<div class="col">
-		<img class="img-fluid" src="/imgs/{event.thumbnail}" alt={event.thumbnail} />
+		<img
+			class="img-fluid"
+			src="/imgs/{event.thumbnail != null ? event.thumbnail : 'placeholder.png'}"
+			alt="event thumbnail"
+		/>
 	</div>
 	<div class="col">
 		<h1 class="neue-bold">{event.label}</h1>
@@ -25,9 +39,13 @@
 		<p class="neue"><i class="bi bi-ticket-perforated pe-2" />{event.cash},-&nbsp;Kč</p>
 		<p class="neue"><i class="bi bi-door-open pe-2" />{event.doors}</p>
 		<hr class="border-2" />
-		<a href={event.fbEvent} class="btn btn-outline-primary neue"
-			><i class="pe-2 bi bi-facebook" />Událost</a
+		<a
+			href={event.fbEvent}
+			class="btn btn-outline-primary neue"
+			class:disabled={event.fbEvent == null}
 		>
+			<i class="pe-2 bi bi-facebook" />Událost {event.fbEvent != null ? '' : ' již brzy'}
+		</a>
 		{#if event.tickets != null}
 			<a href={event.tickets} class="btn btn-outline-secondary neue"
 				><i class="pe-2 bi bi-ticket-perforated-fill" />Předprodej lístků</a
@@ -41,10 +59,10 @@
 		font-family: 'NeueMachina';
 		src: url('/neueMachina/NeueMachina-Regular.otf');
 	}
-    
+
 	@font-face {
-        font-family: 'NeueMachina';
-        src: url('/neueMachina/NeueMachina-Ultrabold.otf');
+		font-family: 'NeueMachina';
+		src: url('/neueMachina/NeueMachina-Ultrabold.otf');
 		font-weight: bold;
 	}
 
@@ -54,6 +72,6 @@
 
 	.neue-bold {
 		font-family: 'NeueMachina';
-        font-weight: bold;
+		font-weight: bold;
 	}
 </style>
