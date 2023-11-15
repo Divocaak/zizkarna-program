8e1f2b1a513b01bbfe6adfad8b5ec1ddfb18c0d7
Vojtěch divoký <divokyvojta@gmail.com>
2023-11-15 16:17:03 +0100

    changelog #41

---
+++ src/routes/(designed)/changelog/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+	import snarkdown from 'snarkdown';
+
+	let md = `
+      # Hello
+  
+      ## How are you?
+  
+      This text is _bold_
+    `;
+</script>
+
+<div>
+    {@html snarkdown(md)}
+</div>
+
+# 15.11.23<br />
+<br />
+## přidáno<br />
+<br />
+- changelog<br />
+- do copy eventu přidáno eventDesc<br />
+- goout cena na kartě a v detailu<br />
+<br />
+## upraveno<br />
+- předpordej ikona<br />
+- dlouhé texty zarovnány doleva<br />
+- upraven admin ssh command<br />
+- seo prvky<br />
+- band imgs placeholder<br />
+<br />
+## opraveno - clickable karty<br />
+- placeholder event imgs<br />
+<br />
+# 6.9.23<br />
+<br />
+## opraveno<br />
+- detail na rozkliku<br />
+- desc kapely limit v adminu nesedí s limitem v db<br />
+<br />
+# 4.9.23<br />
+<br />
+## přidáno<br />
+- soundcloud social button<br />
+<br />
+## upraveno - clickable event card => detail page<br />
+<br />
+# 4.7.23<br />
+<br />
+## přidáno<br />
+- fb event copy<br />
+- inbudejovice copy<br />
+<br />
+## upraveno<br />
+- formát času (bez sekund)<br />
+- websafe default font<br />
+<br />
+## opraveno<br />
+- bilej pruh po stranach<br />
+<br />
+# 19.6.23<br />
+<br />
+## přidáno<br />
+- downloadable presskit a fb cover<br />
+<br />
+# 14.6.23<br />
+<br />
+## opraveno<br />
+- add to cal btns opravené datumy<br />
+<br />
+# 9.5.23<br />
+<br />
+## upraveno<br />
+- db pool<br />
