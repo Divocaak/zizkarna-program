a149f3b223cf6d1b51ff1d1707f194fa3112dea5
Vojtěch divoký <divokyvojta@gmail.com>
2023-12-06 21:23:42 +0100

    getting data from db fro main page, sorting wip

--- src/routes/(designed)/+page.server.js
+++ src/routes/(designed)/+page.server.js
@@ -1,15 +1,42 @@
-export const load = async ({params, fetch}) => {
+export const load = async ({ params, fetch }) => {
 
     const result = await fetch("/api/eventsAll");
     const data = await result.json();
 
-    console.log(data);
-    /* data.forEach(event => {
-        console.log(event);
-        console.log(event.date);
-    }); */
+    //const today = new Date("2023-12-01T18:03:00Z").toISOString();
+    const today = new Date("2023-07-07").toISOString();
+    let past = data;
+    let future = [];
+    let close = [];
+    past.forEach(event => {
+        if (event.date < today){
+            console.log("past: " + event.label);
+            return;
+        }
+        
+        past.splice(past.indexOf(event), 1);
+
+        if (close.length < 3) {
+            console.log("close: " + event.label);
+            close.push(event);
+            return;
+        }
+        
+        console.log("future: " + event.label);
+        future.push(event);
+    });
+
+    console.log("---");
+    console.log("all");
+    console.log(data.length);
+    console.log("future");
+    console.log(future.length);
+    console.log("past");
+    console.log(past.length);
+    console.log("close");
+    console.log(close.length);
 
     return {
-        events: data
+        events: []
     }
 }
--- src/routes/api/eventsAll/+server.js
+++ src/routes/api/eventsAll/+server.js
@@ -1,22 +1,24 @@
 import { pool } from "$lib/db/mysql.js";
 
 export async function GET() {
-
-    let result = [];
-    // TODO limit
-    await pool.promise()
-        .query("SELECT id, label, date, doors, cash, presalePrice, fbEvent, tickets FROM event WHERE is_visible IS TRUE ORDER BY date ASC LIMIT 2")
-        .then(([rows, fields]) => rows.forEach(async row => result.push(await getTags(row)))
-        );
+    const [rows, fields] = await pool.promise().query("SELECT id, label, date, doors, cash, presalePrice, fbEvent, tickets FROM event WHERE is_visible IS TRUE ORDER BY date DESC");
+    
+    const result = await Promise.all(rows.map(row => getTags(row)));
+    result.sort((a, b) => new Date(b.date) - new Date(a.date));
 
     return new Response(JSON.stringify(result));
 }
 
 async function getTags(event) {
     event["tags"] = [];
-    await pool.promise().query("SELECT t.label, t.bgColor, t.textColor FROM tag_in_event tib INNER JOIN tag t ON tib.id_tag=t.id WHERE tib.id_event = ?;", event["id"])
-        .then(([rows, fields]) => rows.forEach(row => event["tags"].push(row)));
-    await pool.promise().query("SELECT t.label, t.bgColor, t.textColor FROM band_in_event bie INNER JOIN tag_in_band tie ON bie.id_band=tie.id_band INNER JOIN tag t ON tie.id_tag=t.id WHERE bie.id_event = ?;", event["id"])
-        .then(([rows, fields]) => rows.forEach(row => event["tags"].push(row)));
+
+    const tagQuery = "SELECT t.label, t.bgColor, t.textColor FROM tag_in_event tib INNER JOIN tag t ON tib.id_tag=t.id WHERE tib.id_event = ?;";
+    const [tagRows, tagFields] = await pool.promise().query(tagQuery, event["id"]);
+    event["tags"] = event["tags"].concat(tagRows);
+
+    const bandQuery = "SELECT t.label, t.bgColor, t.textColor FROM band_in_event bie INNER JOIN tag_in_band tie ON bie.id_band=tie.id_band INNER JOIN tag t ON tie.id_tag=t.id WHERE bie.id_event = ?;";
+    const [bandRows, bandFields] = await pool.promise().query(bandQuery, event["id"]);
+    event["tags"] = event["tags"].concat(bandRows);
+
     return event;
 }
