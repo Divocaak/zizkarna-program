bbd56387af0c4f56c23dc392cffb8f13ac0ccecc
Vojtěch divoký <divokyvojta@gmail.com>
2023-12-08 20:38:56 +0100

    add to cal btn fix #57

--- src/lib/AddToCalButtons.svelte
+++ src/lib/AddToCalButtons.svelte
@@ -1,42 +1,31 @@
 <script>
-    export let label, date, doors;
-    import 'add-to-calendar-button';
-    let dateToUse = new Date(date).toLocaleDateString("sv");
+	import { onMount } from 'svelte';
+
+	export let label, date, doors;
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = 'https://cdn.addevent.com/libs/atc/1.6.1/atc.min.js';
+		script.async = true;
+		script.defer = true;
+		document.head.appendChild(script);
+	});
+
+	const dateDate = new Date(date);
+	const year = dateDate.getFullYear();
+	const month = (dateDate.getMonth() + 1).toString().padStart(2, '0');
+	const day = dateDate.getDate().toString().padStart(2, '0');
+	const dateToUse = day + '/' + month + '/' + year;
 </script>
 
-<div class="mt-1">
-    <i class="bi bi-google pe-2 d-inline-flex">
-        <add-to-calendar-button
-            name={label}
-            options="Google"
-            location="Žižkárna, Žižkova tř. 171/28, České Budějovice 6, 370 04 České Budějovice, Česko"
-            startDate={dateToUse}
-            endDate={dateToUse}
-            startTime={doors}
-            endTime="23:59"
-            timeZone="Europe/Prague"
-            buttonStyle="date"
-            listStyle="modal"
-            language="cs"
-            size=1
-            hideCheckmark
-        />
-    </i>
-    <i class="bi bi-apple pe-2 d-inline-flex">
-        <add-to-calendar-button
-            name={label}
-            options="Apple"
-            location="Žižkárna, Žižkova tř. 171/28, České Budějovice 6, 370 04 České Budějovice, Česko"
-            startDate={dateToUse}
-            endDate={dateToUse}
-            startTime={doors}
-            endTime="23:59"
-            timeZone="Europe/Prague"
-            buttonStyle="date"
-            listStyle="modal"
-            language="cs"
-            size=1
-            hideCheckmark
-        />
-    </i>
+<div title="Přidat do mého kalendáře" class="addeventatc">
+	Přidat do mého kalendáře
+	<span class="start">{dateToUse + doors}</span>
+	<span class="end">{dateToUse}</span>
+	<span class="date_format">DD/MM/YYYY</span>
+	<span class="timezone">Europe/Prague</span>
+	<span class="title">Žižkárna: {label}</span>
+	<span class="location">
+		Žižkárna, Žižkova tř. 171/28, České Budějovice 6, 370 04 České Budějovice, Česko
+	</span>
 </div>
