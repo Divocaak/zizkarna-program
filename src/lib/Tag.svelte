323b26ac76fe5a1c5de9222cda767a15814218e0
Vojtěch divoký <divokyvojta@gmail.com>
2023-12-08 12:26:43 +0100

    tag outline #45

--- src/lib/Tag.svelte
+++ src/lib/Tag.svelte
@@ -4,7 +4,7 @@
 
 <span
 	class="badge bg-secondary mx-1 neue"
-	style="background-color:#{tag.bgColor}!important;color:#{tag.textColor}!important;"
+	style="background-color:#{tag.bgColor}!important;color:#{tag.textColor}!important;border:2.5px solid #{tag.textColor};"
 >
 	{tag.label}
 </span>
