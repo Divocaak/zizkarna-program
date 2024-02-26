b3654a42e60d75ce45b6f8a040b3997f899c1d56
Vojtěch divoký <divokyvojta@gmail.com>
2024-02-26 22:07:32 +0100

    insta story generator #91

--- src/routes/admin/events/video/[id]/+page.svelte
+++ src/routes/admin/events/video/[id]/+page.svelte
@@ -1,20 +1,26 @@
 <script>
+	import { writable } from 'svelte/store';
 	export let data;
+
+	const isLoading = writable(false);
+
 	const event = data.event;
 	const eventTags = data.eventTags;
 	const bands = data.bands ?? [];
 
 	async function sendData(selectedBand) {
-		const response = await fetch('/api/videoGenerator', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ event: event, eventTags: eventTags, band: selectedBand })
-		});
+		try {
+			isLoading.set(true);
 
-		if (response.ok) {
-			console.log('ok');
-		} else {
-			console.error('Failed to store data on the server');
+			const response = await fetch('/api/videoGenerator', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ event: event, eventTags: eventTags, band: selectedBand })
+			});
+		} catch (error) {
+			console.error('Error:', error);
+		} finally {
+			isLoading.set(false);
 		}
 	}
 </script>
@@ -22,19 +28,19 @@
 <a href="/admin/events">zpět</a><br /><br />
 {#each bands as band}
 	<button on:click={() => sendData(band)}>{band.label}</button><br />
-{/each}
+{/each}<br>
 
-<!-- svelte-ignore a11y-media-has-caption -->
-<video width="1080" height="1920" autoplay loop>
-	<!-- BUG not found -->
-	<source src="/dynamic/generator/video.mp4" type="video/mp4" />
-</video>
+{#if $isLoading}
+	vytvářím video..
+{:else}
+	<!-- svelte-ignore a11y-media-has-caption -->
+	<video width="342" height="607" autoplay loop>
+		<source src="/dynamic/generator/video.mp4" type="video/mp4" />
+	</video>
+{/if}
 
 <style>
 	video {
-		position: absolute;
-		top: -70%;
 		border: 2px solid red;
-		transform: scale(0.3);
 	}
 </style>
--- src/routes/api/videoGenerator/+server.js
+++ src/routes/api/videoGenerator/+server.js
@@ -3,7 +3,33 @@ import ffmpegStatic from 'ffmpeg-static';
 import ffmpeg from 'fluent-ffmpeg';
 import { Canvas, loadImage, registerFont } from 'canvas';
 
+const outputPath = "dynamic/generator";
+
+const frameRate = 30;
+
+const crossfadeTime = .1;
+const fadeTime = .8;
+const sectionLen = 2;
+
+const eventIn = 0;
+const eventContent = eventIn + fadeTime;
+const eventOut = eventContent + sectionLen;
+const eventEnd = eventOut + fadeTime;
+
+const bandIn = eventOut + crossfadeTime;
+const bandContent = bandIn + fadeTime;
+const bandOut = bandContent + sectionLen;
+const bandEnd = bandOut + fadeTime;
+
+const zzIn = bandOut + crossfadeTime;
+const zzContent = zzIn + fadeTime;
+
+const duration = zzContent + sectionLen;
+
 export async function POST({ request }) {
+
+    registerFont('static/neueMachina/NeueMachina-Regular.otf', { family: 'Neue Machina' })
+    registerFont('static/Karla-VariableFont_wght.ttf', { family: 'Karla' })
 
     const data = await request.json();
     const event = data.event;
@@ -13,11 +39,7 @@ export async function POST({ request }) {
     const canvas = new Canvas(1080, 1920);
     const context = canvas.getContext('2d');
 
-    const duration = 12;
-    const frameRate = 1;
     const frameCount = Math.floor(duration * frameRate);
-
-    const outputPath = "dynamic/generator";
 
     ffmpeg.setFfmpegPath(ffmpegStatic);
 
@@ -27,31 +49,36 @@ export async function POST({ request }) {
         await fs.promises.mkdir(path, { recursive: true });
     }
 
-    const eventLabelWrapped = getWrappedText(event.label, 180, context, 70);
-    
+    const eventLabelWrapped = getWrappedText(event.label, 120, context, 70);
     const eventTagsWrapped = getWrappedText(getTagsString(eventTags), 300, context, 40);
-
     const poster = await loadImage(`dynamic/events/${event.id}.jpg`);
     const posterDimensions = getImgDimensions(poster, "contain", 1080, 1500);
 
-    const bandLabelWrapped = getWrappedText(band.label, 190, context, 60);
-    const bandDescWrapped = getWrappedText(band.description, 250, context, 50);
-
+    const bandLabelWrapped = getWrappedText(band.label, 160, context, 60);
+    const bandDescWrapped = getWrappedText(band.description, 220, context, 50);
+    const bandTagsWrapped = getWrappedText(getTagsString(band.tags), 300, context, 40);
     const bandImage = await loadImage(`dynamic/bands/${band.id}/${band.imgs[0]}`);
     const bandImageDimensions = getImgDimensions(bandImage, "contain", 1080, 1500);
+    const bandStageTimeWrapped = getWrappedText(`Stage time: ${band.stageTime.substring(0, band.stageTime.length - 3)}`, 300, context, 40);
 
-    const bandTagsWrapped = getWrappedText(getTagsString(band.tags), 300, context, 40);
+    let dateFormatted = new Date(event.date).toLocaleDateString('cs-CZ', {
+        month: 'numeric',
+        day: 'numeric',
+        weekday: 'long'
+    });
+    const dateWrapped = getWrappedText(dateFormatted, 500, context, 90);
+    const doorsWrapped = getWrappedText(`otevřeno od ${event.doors.substring(0, event.doors.length - 3)}`, 500, context, 90);
+    const logo = await loadImage(`static/logo_transparent.png`);
 
     // Render each frame
     for (let i = 0; i < frameCount; i++) {
         const time = i / frameRate;
 
         // clear canvas
-        /* context.fillStyle = '#1f1f1f'; */
-        context.fillStyle = 'darkslategray';
+        context.fillStyle = '#1f1f1f';
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        renderFrame(context, time, poster, posterDimensions, eventLabelWrapped, eventTagsWrapped, bandLabelWrapped, bandDescWrapped, bandImage, bandImageDimensions, bandTagsWrapped);
+        renderFrame(context, time, poster, posterDimensions, eventLabelWrapped, eventTagsWrapped, bandLabelWrapped, bandDescWrapped, bandImage, bandImageDimensions, bandTagsWrapped, bandStageTimeWrapped, logo, dateWrapped, doorsWrapped);
 
         // Store the image in the directory where it can be found by FFmpeg
         const output = canvas.toBuffer('image/png');
@@ -69,85 +96,125 @@ export async function POST({ request }) {
     return new Response(JSON.stringify({ message: outputPath }, { status: 200 }));
 }
 
-function renderFrame(context, time, poster, posterDimensions, eventLabel, eventTags, bandLabel, bandDesc, bandImage, bandImageDimensions, bandTags) {
+function renderFrame(context, time, poster, posterDimensions, eventLabel, eventTags, bandLabel, bandDesc, bandImage, bandImageDimensions, bandTags, bandStageTime, logo, date, doors) {
 
     context.fillStyle = "#d4d4d4";
 
-    /* TODO easing (work with greater framerate */
-    // { time: 3, value: 200, easing: 'cubic-in-out' },
+    /* event section */
+
     const eventLabelX = interpolateKeyframes([
-        { time: 0, value: 140 },
-        { time: 2.5, value: 140 },
-        { time: 3, value: 140 },
-        { time: 5, value: -1400 }
+        { time: eventOut, value: 100 },
+        { time: eventEnd, value: -1400 }
     ], time);
     const eventLabelY = interpolateKeyframes([
-        { time: 0, value: -200 },
-        { time: 2.5, value: 250 }
+        { time: eventIn, value: -200 },
+        { time: eventContent, value: 250 }
     ], time);
-    context.font = "70px serif";
+    context.font = "70px Neue Machina";
     eventLabel.forEach(function (item) {
         context.fillText(item[0], eventLabelX, eventLabelY + item[1]);
     });
 
     const posterX = interpolateKeyframes([
-        { time: 0, value: 0 },
-        { time: 2.5, value: 0 },
-        { time: 3, value: 0 },
-        { time: 5, value: -1400 }
+        { time: eventIn, value: -1400 },
+        { time: eventContent, value: 0 },
+        { time: eventOut, value: 0 },
+        { time: eventEnd, value: -1400 }
     ], time);
-    const posterY = interpolateKeyframes([
-        { time: 0, value: 2000 },
-        { time: 2.5, value: 500 }
-    ], time);
-    context.drawImage(poster, posterX, posterY, posterDimensions.w, posterDimensions.h);
+    context.drawImage(poster, posterX, 500, posterDimensions.w, posterDimensions.h);
 
     const eventTagsX = interpolateKeyframes([
-        { time: 3.5, value: 140 },
-        { time: 5, value: -1400 }
+        { time: eventOut, value: 140 },
+        { time: eventEnd, value: -1400 }
     ], time);
     const eventTagsY = interpolateKeyframes([
-        { time: 0.5, value: 2000 },
-        { time: 2.5, value: 1500 }
+        { time: eventIn, value: 2000 },
+        { time: eventContent, value: 1500 }
     ], time);
-    context.font = "30px serif";
+    context.font = "40px Neue Machina";
     eventTags.forEach(function (item) {
         context.fillText(item[0], eventTagsX, eventTagsY + item[1]);
     });
 
+    /* band section */
+
     const bandLabelX = interpolateKeyframes([
-        { time: 3.5, value: 1080 },
-        { time: 5, value: 140 }
+        { time: bandIn, value: 1080 },
+        { time: bandContent, value: 140 },
+        { time: bandOut, value: 140 },
+        { time: bandEnd, value: -1400 },
     ], time);
-    context.font = "60px serif";
+    context.font = "70px Neue Machina";
     bandLabel.forEach(function (item) {
         context.fillText(item[0], bandLabelX, 150 + item[1]);
     });
 
     const bandDescX = interpolateKeyframes([
-        { time: 4, value: 1080 },
-        { time: 5.5, value: 100 }
+        { time: bandIn + .1, value: 1080 },
+        { time: bandContent, value: 100 },
+        { time: bandOut, value: 100 },
+        { time: bandEnd, value: -1400 },
     ], time);
-    /* TODO different font for description */
-    context.font = "40px serif";
+    context.font = "40px Karla";
     bandDesc.forEach(function (item) {
         context.fillText(item[0], bandDescX, 300 + item[1]);
     });
 
-    const bandTagsX = interpolateKeyframes([
-        { time: 3.5, value: 1080 },
-        { time: 5, value: 140 }
+    const bandStageTimeX = interpolateKeyframes([
+        { time: bandIn + .2, value: 1080 },
+        { time: bandContent, value: 140 },
+        { time: bandOut, value: 140 },
+        { time: bandEnd, value: -1400 },
     ], time);
-    context.font = "30px serif";
+    bandStageTime.forEach(function (item) {
+        context.fillText(item[0], bandStageTimeX, 950 + item[1]);
+    });
+
+    const bandTagsX = interpolateKeyframes([
+        { time: bandIn + .3, value: 1080 },
+        { time: bandContent, value: 140 },
+        { time: bandOut, value: 140 },
+        { time: bandEnd, value: -1400 },
+    ], time);
+    context.font = "40px Neue Machina";
     bandTags.forEach(function (item) {
-        context.fillText(item[0], bandTagsX, 1000 + item[1]);
+        context.fillText(item[0], bandTagsX, 1050 + item[1]);
     });
 
     const bandImageX = interpolateKeyframes([
-        { time: 4.5, value: 1080 },
-        { time: 6, value: 0 }
+        { time: bandOut, value: 0 },
+        { time: bandEnd, value: -1400 }
     ], time);
-    context.drawImage(bandImage, bandImageX, 1920 - bandImageDimensions.h, bandImageDimensions.w, bandImageDimensions.h);
+    const bandImageY = interpolateKeyframes([
+        { time: bandIn, value: 2000 },
+        { time: bandContent, value: 1920 - (bandImageDimensions.h) }
+    ], time);
+    context.drawImage(bandImage, bandImageX, bandImageY, bandImageDimensions.w, bandImageDimensions.h);
+
+    /* zz section */
+
+    const logoY = interpolateKeyframes([
+        { time: zzIn + .2, value: -1080 },
+        { time: zzContent, value: 150 }
+    ], time);
+    context.drawImage(logo, 0, logoY, 1080, 1080);
+
+    const dateX = interpolateKeyframes([
+        { time: zzIn, value: 1080 },
+        { time: zzContent, value: 250 }
+    ], time);
+    context.font = "60px Neue Machina";
+    date.forEach(function (item) {
+        context.fillText(item[0], dateX, 1300 + item[1]);
+    });
+
+    const doorsX = interpolateKeyframes([
+        { time: zzIn + .1, value: 1080 },
+        { time: zzContent, value: 250 }
+    ], time);
+    doors.forEach(function (item) {
+        context.fillText(item[0], doorsX, 1500 + item[1]);
+    });
 }
 
 function getTagsString(tags) { return tags.map(tag => tag.label).join('').replaceAll("////", "//"); }
@@ -170,14 +237,15 @@ function getWrappedText(text, maxWidth, context, lineHeight) {
     words.forEach(word => {
         let testLine = line + word + ' ';
         let testLineWidth = context.measureText(testLine).width;
-        /* TODO refactor condition */
-        if (testLineWidth > maxWidth) {
-            lineArray.push([line.trim(), y]);
-            line = word + ' ';
-            y += lineHeight;
-        } else {
+        
+        if (testLineWidth <= maxWidth) {
             line = testLine;
+            return;
         }
+
+        lineArray.push([line.trim(), y]);
+        line = word + ' ';
+        y += lineHeight;
     });
 
     lineArray.push([line.trim(), y]);
@@ -202,21 +270,19 @@ function interpolateKeyframes(keyframes, time) {
     const keyframe2 = keyframes[index + 1];
 
     let t = (time - keyframe1.time) / (keyframe2.time - keyframe1.time);
-
-    /* TODO diff approach */
-    if (keyframe2.easing === 'expo-out') {
-        t = applyExponentialOutEasing(t);
-    } else if (keyframe2.easing === 'cubic-in-out') {
-        t = applyCubicInOutEasing(t);
-    } else {
-        // ... Implement more easing functions
-    }
+    t = easeInOutBack(t);
 
     return keyframe1.value + (keyframe2.value - keyframe1.value) * t;
 }
 
-const applyExponentialOutEasing = t => t === 1 ? 1 : 1 - 2 ** (-10 * t);
-const applyCubicInOutEasing = t => t < 0.5 ? 4 * t ** 3 : 1 - ((-2 * t + 2) ** 3) / 2;
+function easeInOutBack(x) {
+    const c1 = 1.70158;
+    const c2 = c1 * 1.525;
+
+    return x < 0.5
+        ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+        : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+}
 
 async function stitchFramesToVideo(
     framesFilepath,
