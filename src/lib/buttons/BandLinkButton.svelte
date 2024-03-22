e515fe62f996cfbf7d455817ec7f45979df3fe67
Vojtěch divoký <divokyvojta@gmail.com>
2024-03-22 23:10:43 +0100

    support tiktok as bandlink #127

--- src/lib/buttons/BandLinkButton.svelte
+++ src/lib/buttons/BandLinkButton.svelte
@@ -8,6 +8,7 @@
 		Facebook: 'Facebook',
 		Instagram: 'Instagram',
 		SoundCloud: 'SoundCloud',
+		TikTok: "TikTok",
 		Other: 'Jiný odkaz'
 	};
 
@@ -39,6 +40,9 @@
 		case 'soundcloud.com':
 			type = Types.SoundCloud;
 			break;
+		case 'tiktok.com':
+			type = Types.TikTok;
+			break;
 		default:
 			type = Types.Other;
 			break;
@@ -50,7 +54,8 @@
 		[Types.AppleMusic]: { icon: 'bi-music-note-beamed', class: 'applemusic' },
 		[Types.Facebook]: { icon: 'bi-facebook', class: 'facebook' },
 		[Types.Instagram]: { icon: 'bi-instagram', class: 'instagram' },
-		[Types.SoundCloud]: { icon: 'bi-cloud-fill', class: 'soundcloud' }
+		[Types.SoundCloud]: { icon: 'bi-cloud-fill', class: 'soundcloud' },
+		[Types.TikTok]: { icon: 'bi-tiktok', class: 'tiktok' }
 	};
 
 	const typeInfo = typeData[type] || { icon: 'bi-link-45deg', class: '' };
@@ -143,6 +148,19 @@
 
 	.soundcloud:hover {
 		background-color: #ff7700;
+		color: #f8f9fa;
+	}
+
+	.tiktok,
+	.tiktok:active,
+	.tiktok:visited,
+	.tiktok:focus {
+		border-color: #000000;
+		color: #000000;
+	}
+
+	.tiktok:hover {
+		background-color: #000000;
 		color: #f8f9fa;
 	}
 </style>
