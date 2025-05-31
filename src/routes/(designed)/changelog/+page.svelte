56660380242cd0660c623a3b4411c5e629fd0f96
Vojtěch divoký <divokyvojta@gmail.com>
2025-05-31 18:11:59 +0200

    vystavovani

--- src/routes/(designed)/changelog/+page.svelte
+++ src/routes/(designed)/changelog/+page.svelte
@@ -2,6 +2,12 @@
     import SvelteMarkdown from 'svelte-markdown';
     const source = `
 
+# 31. 5. 25
+
+## přidáno
+- API endpoint pro Guilde (integrace do inbudejovice.cz)
+
+
 # 20. 1. 25
 
 ## upraveno
